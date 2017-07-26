e908c95cf243c84ec8f451ffe82ee1e93c40c563
Zac Hancock <zshancock@gmail.com>
2017-07-26 15:25:27 -0600

    Update and rename practice_math_package.py to circumference_of_a_circle.py

--- practice_math_package.py
+++
@@ -1,6 +0,0 @@
-## Generates the circumference of a circle with a radius of 13 units and rounds to 3 places. ##
-
-import math
-radius = 13
-C = round(2 * math.pi * radius, 3)
-print "The circumference of a circle with a radius of 13 units is " + str(C) + " units."
