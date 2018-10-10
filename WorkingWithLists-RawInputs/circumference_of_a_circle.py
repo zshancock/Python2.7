659e315b7f43916cc2aeb232b49c7bd39f671b77
Zac Hancock <zshancock@gmail.com>
2018-10-10 09:20:03 -0400

    Delete circumference_of_a_circle.py

--- WorkingWithLists-RawInputs/circumference_of_a_circle.py
+++
@@ -1,6 +0,0 @@
-## Generates the circumference of a circle with a radius variable (can be changed) units and rounds to 3 places. ##
-
-import math
-radius = 13  # can be changed
-C = round(2 * math.pi * radius, 3)
-print "The circumference of a circle with a radius of " + str(radius) + " units is " + str(C) + " units."
